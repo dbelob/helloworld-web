b47b4696864f904ed5a284a08efdccbc2ae2c517
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-06-20 12:52:06 +0300

    Configure with Java configuration

--- helloworld-web-spring-boot/src/main/java/acme/hello/App.java
+++ helloworld-web-spring-boot/src/main/java/acme/hello/App.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
@@ -14,5 +17,13 @@ public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 }
