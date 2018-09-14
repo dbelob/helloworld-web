f0b9e310614cf616d5971db51890869ea4bc1aff
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-09-14 12:54:46 +0300

    Add Micronaut tests

---
+++ helloworld-web-micronaut/src/test/java/acme/hello/HelloClientTest.java
@@ -0,0 +1,16 @@
+package acme.hello;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloClientTest {
+    @Test
+    public void clientTest() {
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer.class);
+        HelloClient client = embeddedServer.getApplicationContext().getBean(HelloClient.class);
+
+        Assert.assertEquals("Hello, world!", client.hello().blockingGet());
+    }
+}
---
+++ helloworld-web-micronaut/src/test/java/acme/hello/HelloControllerTest.java
@@ -0,0 +1,18 @@
+package acme.hello;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloControllerTest {
+    @Test
+    public void serverTest() {
+        EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer.class);
+        HttpClient client = HttpClient.create(embeddedServer.getURL());
+
+        Assert.assertEquals("Hello, world!", client.toBlocking().retrieve(HttpRequest.GET("/")));
+    }
+}
