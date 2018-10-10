9048ec4ca91504c626d83ae79d122db0d6181d7a
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-10-10 17:03:51 +0300

    Change RESTEasy example

--- helloworld-web-jaxrs-resteasy/src/main/java/acme/hello/HelloApplication.java
+++ helloworld-web-jaxrs-resteasy/src/main/java/acme/hello/HelloApplication.java
@@ -1,8 +1,24 @@
 package acme.hello;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
-@ApplicationPath("/")
 public class HelloApplication extends Application {
+    private Set<Object> singletons = new HashSet<>();
+    private Set<Class<?>> empty = new HashSet<>();
+
+    public HelloApplication() {
+        singletons.add(new HelloResource());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return empty;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
 }
