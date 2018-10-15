06a85a9c8ab95beb33db0306b580c66a1532202e
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-10-15 14:49:12 +0300

    Change Grails example

---
+++ helloworld-web-grails/grails-app/conf/UrlMappings.groovy
@@ -0,0 +1,15 @@
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?" {
+            constraints {
+                // apply constraints here
+            }
+        }
+
+//        "/"(view:"/index")
+        "/"(controller: 'hello', action: 'index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
+    }
+}
---
+++ helloworld-web-grails/grails-app/views/index.gsp
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+    %{--<meta name="layout" content="main"/>--}%
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>Grails</p>
+</body>
+</html>
