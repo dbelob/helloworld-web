d6ca57aa2b5229b919070cffcd692213234977eb
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-11-05 23:22:50 +0300

    Add Open Liberty example

---
+++ helloworld-web-microprofile-openliberty/src/main/java/acme/hello/HelpApplication.java
@@ -0,0 +1,10 @@
+package acme.hello;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationScoped
+@ApplicationPath("/")
+public class HelpApplication extends Application {
+}
