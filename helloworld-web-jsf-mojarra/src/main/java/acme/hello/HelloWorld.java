95a39f1f32e6778190253f50c78f5528f7181699
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-09-17 20:44:40 +0300

    Change comments

--- helloworld-web-jsf-mojarra/src/main/java/acme/hello/HelloWorld.java
+++ helloworld-web-jsf-mojarra/src/main/java/acme/hello/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
     /**
      * Determines the JSF specification version
      *
-     * @return JSF Specification version, e.g. 2.1
+     * @return JSF Specification version
      */
     public String getSpecificationVersion() {
         return FacesContext.getCurrentInstance().getClass().getPackage().getSpecificationVersion();
@@ -19,7 +19,7 @@ public class HelloWorld {
     /**
      * Determines the JSF implementation version
      *
-     * @return JSF Implementation version, e.g. 2.1.26
+     * @return JSF Implementation version
      */
     public String getImplementationVersion() {
         return FacesContext.getCurrentInstance().getClass().getPackage().getImplementationVersion();
