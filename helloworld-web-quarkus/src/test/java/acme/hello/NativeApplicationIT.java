62421c8f73c586469ffc50b09650029072ac7c20
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-09-27 12:32:04 +0300

    Add Quarkus example

---
+++ helloworld-web-quarkus/src/test/java/acme/hello/NativeApplicationIT.java
@@ -0,0 +1,9 @@
+package acme.hello;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeApplicationIT extends ApplicationTest {
+
+    // Execute the same tests but in native mode.
+}
